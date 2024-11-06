f0f0e56605d8bbd31370cbfc129eae0e2862b9e9
Amrita A Thirumalai <aathiru@umich.edu>
2024-11-05 22:09:04 -0500

    modified to use a generate block

--- verilog/cdb.sv
+++ verilog/cdb.sv
@@ -2,9 +2,9 @@
 
 typedef struct packed {
     PHYS_REG_IDX reg_idx;
-    // some reg_val;
+    DATA reg_val;
     logic valid;
-} CDB;
+} CDB_PACKET;
 
 module CDB #(
     parameter N = `N
@@ -12,19 +12,13 @@ module CDB #(
 (
     input logic         [NUM_FU-1:0] fu_done,
     input FU_PACKET     [NUM_FU-1:0] wr_data,
-    
-    input logic                      br_done,
-    input FU_PACKET                  br_data,
 
-    output CDB_PACKET [N-1:0]        entries;
-    output CDB_PACKET                br_out;
-    output logic      [NUM_FU-1:0]   stall_sig;          
+    output CDB_PACKET   [N-1:0]      entries;
+    output logic        [NUM_FU-1:0] stall_sig;          
 
 );
-    localparam NUM_FU = `NUM_FU_ALU + `NUM_FU_MULT + `NUM_FU_LOAD + `NUM_FU_STORE;
-    localparam FU_PACKET_WIDTH = $bits(br_data);
-    
-    CDB_PACKET [N-1:0] entries;
+    localparam NUM_FU = `NUM_FU_ALU + `NUM_FU_MULT + `NUM_FU_LOAD + `NUM_FU_STORE + `NUM_FU_BR;
+    localparam FU_PACKET_WIDTH = $bits(wr_data[0]);
 
     logic [NUM_FU-1:0] cdb_gnt;
     logic [N-1:0][NUM_FU-1:0] cdb_gnt_bus;
@@ -34,27 +28,31 @@ module CDB #(
 
     psel_gen #(
         .WIDTH(NUM_FU),
-        .REQS(num_req)) 
-    alu_psel (
+        .REQS(N)) 
+    cdb_arb (
         .req(fu_done),
         .gnt(cdb_gnt),
         .gnt_bus(cdb_gnt_bus),
         .empty()
     );
 
-    logic FU_PACKET [N-1:0][NUM_FU-1:0] anded_packets;
-    logic FU_PACKET [N-1:0] selected_packet;
+    wor FU_PACKET [N-1:0] selected_packets;
 
     generate
         genvar i, j;
-        for (i = 0; i < num_req; i = i + 1) begin
+        for (i = 0; i < N; i = i + 1) begin
             for (j = 0; j < NUM_FU; j = j + 1) begin
-                assign anded_packets[i][j] = wr_data[j] & '{FU_PACKET_WIDTH{one_hot[i][j]}};
+                assign selected_packets[i] = cdb_gnt_bus[i][j] ? wr_data[j] : '0;
             end
-            assign selected_packet[i] = |anded_packets[i];
         end
     endgenerate
     
-    // then set the CDB output packet for both entries and br_out
+    always_comb begin
+        for (int i = 0; i < N; i++) begin
+            entries[i].reg_idx = selected_packets[i].reg_idx;
+            entries[i].reg_val = selected_packets[i].reg_idx;
+            entries[i].valid = (selected_packets[i]) ? 1 : 0;
+        end
+    end
 
 endmodule
