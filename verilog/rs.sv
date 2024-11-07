26f26352dc29daa629542799b99ebe4427450d99
Anup Bagali <anup.bagali1@gmail.com>
2024-11-07 17:33:47 -0500

    Huge RS refactor

--- verilog/rs.sv
+++ verilog/rs.sv
@@ -1,39 +1,39 @@
 `include "sys_defs.svh"
 `include "psel_gen.sv"
+`include "rs_psel.sv"
 
 module RS #(
-    parameter DEPTH = 32,
+    parameter DEPTH = `RS_SZ,
     parameter N = `N
 )
 (
-    input                               clock,
-    input                               reset,
+    input                                                           clock,
+    input                                                           reset,
 
-    input RS_PACKET [N-1:0]             rs_in,
-    input CDB_PACKET [N-1:0]            cdb_in,
+    input RS_PACKET                 [N-1:0]                         rs_in,
+    input CDB_PACKET                [N-1:0]                         cdb_in,
 
     // ebr logic
-    input BR_MASK                       br_id,                   
-    input BR_TASK                       br_task,                   
+    input BR_MASK                                                   br_id,
+    input BR_TASK                                                   br_task,
 
     // busy bits from FUs to mark when available to issue
-    input logic [`NUM_FU_ALU-1:0]          fu_alu_busy,
-    input logic [`NUM_FU_MULT-1:0]         fu_mult_busy,
-    input logic [`NUM_FU_LD-1:0]           fu_ld_busy,
-    input logic [`NUM_FU_STORE-1:0]        fu_store_busy,
-    //input logic [`NUM_FU_BR-1:0]           fu_br_busy, 
+    input logic                     [`NUM_FU_ALU-1:0]              fu_alu_busy,
+    input logic                     [`NUM_FU_MULT-1:0]             fu_mult_busy,
+    input logic                     [`NUM_FU_LD-1:0]               fu_ld_busy,
+    input logic                     [`NUM_FU_STORE-1:0]            fu_store_busy,
+    input logic                     [`NUM_FU_BR-1:0]               fu_br_busy, 
 
-    output logic [$clog2(N+1)-1:0]         num_accept,
+    output logic                    [$clog2(N+1)-1:0]              num_accept,
 
     // output packets directly to FUs (they all are pipelined)
-    output RS_PACKET [`NUM_FU_ALU-1:0]          issued_alu, 
-    output RS_PACKET [`NUM_FU_MULT-1:0]         issued_mult,
-    output RS_PACKET [`NUM_FU_LD-1:0]           issued_ld,
-    output RS_PACKET [`NUM_FU_STORE-1:0]        issued_store,
-    output RS_PACKET                            issued_br,
+    output RS_PACKET                [`NUM_FU_ALU-1:0]              issued_alu, 
+    output RS_PACKET                [`NUM_FU_MULT-1:0]             issued_mult,
+    output RS_PACKET                [`NUM_FU_LD-1:0]               issued_ld,
+    output RS_PACKET                [`NUM_FU_STORE-1:0]            issued_store,
+    output RS_PACKET                                               issued_br,
 
-    output logic [$clog2(DEPTH+1)-1:0]          open_entries,
-
+    output logic                    [$clog2(DEPTH+1)-1:0]          open_entries
 
     `ifdef DEBUG
     ,   output ROB_ENTRY_PACKET [DEPTH-1:0] debug_entries
@@ -41,130 +41,101 @@ module RS #(
 );
     localparam LOG_DEPTH = $clog2(DEPTH);
 
+    logic [DEPTH-1:0] open_spots, next_open_spots;
+    wor [DEPTH-1:0] all_issued_insts; // also keeps track of position of instructions being issued w.r.t RS entries
+
     RS_PACKET [DEPTH-1:0] entries, next_entries;
     logic [LOG_DEPTH:0] num_entries, next_num_entries;
 
-    assign open_entries = DEPTH - num_entries;
+    // grant bus for the psel that selects the open spots that can be dispatched to
+    logic [N-1:0][DEPTH-1:0] dis_entries_bus;
 
     // Issuing psel wires
     logic [DEPTH-1:0] alu_req, mult_req, ld_req, store_req, br_req;
-    logic [DEPTH-1:0] alu_gnt, mult_gnt, ld_gnt, store_gnt, br_gnt;
-    logic [`NUM_FU_ALU-1:0][DEPTH-1:0] alu_gnt_bus
-    logic [`NUM_FU_MULT-1:0][DEPTH-1:0] mult_gnt_bus;
-    logic [`NUM_FU_LOAD-1:0][DEPTH-1:0] ld_gnt_bus;
-    logic [`NUM_FU_STORE-1:0][DEPTH-1:0] store_gnt_bus;
-    logic [`NUM_FU_BR-1:0][DEPTH-1:0] br_gnt_bus;
 
-    // Free psel wires
-    logic [`NUM_FU_ALU-1:0]      f_alu_req, f_alu_gnt;
-    logic [`NUM_FU_MULT-1:0]     f_mult_req, f_mult_gnt;
-    logic [`NUM_FU_LD-1:0]       f_ld_req, f_ld_gnt;
-    logic [`NUM_FU_STORE-1:0]    f_store_req, f_store_gnt;
+    // Which specific FUs are being issued to
+    logic [`NUM_FU_ALU-1:0] alu_issued;
+    logic [`NUM_FU_MULT-1:0] mult_issued;
+    logic [`NUM_FU_LD-1:0] ld_issued;
+    logic [`NUM_FU_STORE-1:0] store_issued;
+    logic [`NUM_FU_BR-1:0] br_issued;
 
-    logic [`NUM_FU_ALU-1:0][`NUM_FU_ALU-1:0]       f_alu_gnt_bus;
-    logic [`NUM_FU_MULT-1:0][`NUM_FU_MULT-1:0]     f_mult_gnt_bus;
-    logic [`NUM_FU_LD-1:0][`NUM_FU_LD-1:0]         f_ld_gnt_bus;
-    logic [`NUM_FU_STORE-1:0][`NUM_FU_STORE-1:0]   f_store_gnt_bus;
+    // Number issued per FU
+    logic [$clog2(`NUM_FU_ALU+1)-1:0] num_alu_issued;
+    logic [$clog2(`NUM_FU_MULT+1)-1:0] num_mult_issued;
+    logic [$clog2(`NUM_FU_LD+1)-1:0] num_ld_issued;
+    logic [$clog2(`NUM_FU_STORE+1)-1:0] num_store_issued;
+    logic [$clog2(`NUM_FU_BR+1)-1:0] num_br_issued;
 
-    // Issuing Priority Selectors, one for each type of FU
-    psel_gen #(
-        .WIDTH(DEPTH),
-        .REQS(`NUM_FU_ALU)) 
+    assign open_entries = DEPTH - num_entries;
+
+    rs_psel #(
+        .DEPTH(DEPTH),
+        .NUM_FU(`NUM_FU_ALU)
+    )
     alu_psel (
-        .req(alu_req),
-        .gnt(alu_gnt),
-        .gnt_bus(alu_gnt_bus),
-        .empty()
+        .inst_req(alu_req),
+        .fu_req(~fu_alu_busy),
+        .num_issued(num_alu_issued),
+        .fu_issued_insts(alu_issued),
+        .all_issued_insts(all_issued_insts)
     );
 
-    psel_gen #(
-        .WIDTH(DEPTH),
-        .REQS(`NUM_FU_MULT)) 
+    rs_psel #(
+        .DEPTH(DEPTH),
+        .NUM_FU(`NUM_FU_MULT)
+    )
     mult_psel (
-        .req(mult_req),
-        .gnt(mult_gnt),
-        .gnt_bus(mult_gnt_bus),
-        .empty()
+        .inst_req(mult_req),
+        .fu_req(~fu_mult_busy),
+        .num_issued(num_mult_issued),
+        .fu_issued_insts(mult_issued),
+        .all_issued_insts(all_issued_insts)
     );
 
-    psel_gen #(
-        .WIDTH(DEPTH),
-        .REQS(`NUM_FU_LD)) 
+    rs_psel #(
+        .DEPTH(DEPTH),
+        .NUM_FU(`NUM_FU_LD)
+    )
     ld_psel (
-        .req(ld_req),
-        .gnt(ld_gnt),
-        .gnt_bus(ld_gnt_bus),
-        .empty()
+        .inst_req(ld_req),
+        .fu_req(~fu_ld_busy),
+        .num_issued(num_ld_issued),
+        .fu_issued_insts(ld_issued),
+        .all_issued_insts(all_issued_insts)
     );
 
-    psel_gen #(
-        .WIDTH(DEPTH),
-        .REQS(`NUM_FU_STORE)) 
+    rs_psel #(
+        .DEPTH(DEPTH),
+        .NUM_FU(`NUM_FU_STORE)
+    )
     store_psel (
-        .req(store_req),
-        .gnt(store_gnt),
-        .gnt_bus(store_gnt_bus),
-        .empty()
+        .inst_req(store_req),
+        .fu_req(~fu_store_busy),
+        .num_issued(num_store_issued),
+        .fu_issued_insts(store_issued),
+        .all_issued_insts(all_issued_insts)
     );
 
-    psel_gen #(
-        .WIDTH(DEPTH),
-        .REQS(`NUM_FU_STORE)) 
-    store_psel (
-        .req(store_req),
-        .gnt(store_gnt),
-        .gnt_bus(store_gnt_bus),
-        .empty()
-    );
-
-    psel_gen #(
-        .WIDTH(DEPTH),
-        .REQS(`NUM_FU_BR)) 
+    rs_psel #(
+        .DEPTH(DEPTH),
+        .NUM_FU(`NUM_FU_BR)
+    )
     br_psel (
-        .req(br_req),
-        .gnt(br_gnt),
-        .gnt_bus(br_gnt_bus),
-        .empty()
-    );
-
-    // Busy Psels
-    psel_gen #(
-        .WIDTH(`NUM_FU_ALU),
-        .REQS(`NUM_FU_ALU)) 
-    f_alu_psel (
-        .req(~fu_alu_busy),
-        .gnt(f_alu_gnt),
-        .gnt_bus(f_alu_gnt_bus),
-        .empty()
+        .inst_req(br_req),
+        .fu_req(~fu_br_busy),
+        .num_issued(num_br_issued),
+        .fu_issued_insts(br_issued),
+        .all_issued_insts(all_issued_insts)
     );
 
     psel_gen #(
-        .WIDTH(`NUM_FU_MULT),
-        .REQS(`NUM_FU_MULT)) 
-    f_mult_psel (
-        .req(~fu_mult_busy),
-        .gnt(f_mult_gnt),
-        .gnt_bus(f_mult_gnt_bus),
-        .empty()
-    );
-
-    psel_gen #(
-        .WIDTH(`NUM_FU_LD),
-        .REQS(`NUM_FU_LD)) 
-    f_ld_psel (
-        .req(~fu_ld_busy),
-        .gnt(f_ld_gnt),
-        .gnt_bus(f_ld_gnt_bus),
-        .empty()
-    );
-
-    psel_gen #(
-        .WIDTH(`NUM_FU_STORE),
-        .REQS(`NUM_FU_STORE)) 
-    f_store_psel (
-        .req(~fu_store_busy),
-        .gnt(f_store_gnt),
-        .gnt_bus(f_store_gnt_bus),
+        .WIDTH(DEPTH),
+        .REQS(N))
+    inst_psel (
+        .req(next_open_spots),
+        .gnt(),
+        .gnt_bus(dis_entries_bus),
         .empty()
     );
 
@@ -179,13 +150,17 @@ module RS #(
             if (entries[i].valid & entries[i].t1.ready & entries[i].t2.ready) begin
                 if (entries[i].fu_type == ALU_INST) begin
                     alu_req[i] = 1;
-                end else if (entries[i].fu_type == MULT_INST) begin
+                end 
+                if (entries[i].fu_type == MULT_INST) begin
                     mult_req[i] = 1;
-                end else if (entries[i].fu_type == LD_INST) begin
+                end 
+                if (entries[i].fu_type == LD_INST) begin
                     ld_req[i] = 1;
-                end else if (entries[i].fu_type == STORE_INST) begin
+                end 
+                if (entries[i].fu_type == STORE_INST) begin
                     store_req[i] = 1;
-                end else if (entries[i].fu_type == BR_INST) begin
+                end  
+                if (entries[i].fu_type == BR_INST) begin
                     br_req[i] = 1;
                 end
             end
@@ -196,6 +171,7 @@ module RS #(
     always_comb begin
         next_entries = entries;
         next_num_entries = num_entries;
+        next_open_spots = open_spots | all_issued_insts;
 
         // Marks entry tags as ready (parallelized)
         for (int i = 0; i < N; i++) begin
@@ -214,117 +190,51 @@ module RS #(
         end
 
         // Branch mask logic
-        if (br_task != NOTHING) begin
-            if (br_task == SQUASH) begin
-                for (int i = 0; i < DEPTH; i++) begin
-                    if ((entries[i].b_mask & br_id) != 0) begin
-                        next_entries[i] = 0;
-                        next_num_entries--;
-                    end
+        if (br_task == SQUASH) begin
+            for (int i = 0; i < DEPTH; i++) begin
+                if ((entries[i].b_mask & br_id) != 0) begin
+                    next_entries[i] = 0;
+                    next_open_spots[i] = 1;
+                    next_num_entries--;
                 end
-            end else if (br_task == CLEAR) begin
-                for (int i = 0; i < DEPTH; i++) begin
-                    if ((entries[i].b_mask & br_id) != 0) begin
-                        next_entries[i].b_mask = entries[i].b_mask ^ br_id;
-                    end
+            end
+        end 
+        if (br_task == CLEAR) begin
+            for (int i = 0; i < DEPTH; i++) begin
+                if ((entries[i].b_mask & br_id) != 0) begin
+                    next_entries[i].b_mask = entries[i].b_mask ^ br_id;
                 end
             end
         end
 
-        // Reads Psel logic and issues (parallelized)
-        for (int i = 0; i < DEPTH; i++) begin
-            for (int j = 0; j < `NUM_FU_ALU; j++) begin
-                if (alu_gnt_bus[j][i]) begin
-                    for (int k = 0; k < `NUM_FU_ALU; k++) begin
-                        if (f_alu_gnt_bus[j][k]) begin
-                            issued_alu[k] = next_entries[i];
-                            next_entries[i] = 0;
-                        end
-                    end
-                end
-            end
-            for (int j = 0; j < `NUM_FU_MULT; j++) begin
-                if (mult_gnt_bus[j][i]) begin
-                    for (int k = 0; k < `NUM_FU_MULT; k++) begin
-                        if (f_mult_gnt_bus[j][k]) begin
-                            issued_mult[k] = next_entries[i];
-                            next_entries[i] = 0;
-                        end
-                    end
-                end
-            end
-            for (int j = 0; j < `NUM_FU_LD; j++) begin
-                if (ld_gnt_bus[j][i]) begin
-                    for (int k = 0; k < `NUM_FU_LD; k++) begin
-                        if (f_ld_gnt_bus[j][k]) begin
-                            issued_ld[k] = next_entries[i];
-                            next_entries[i] = 0;
-                        end
-                    end
-                end
-            end
-            for (int j = 0; j < `NUM_FU_STORE; j++) begin
-                if (store_gnt_bus[j][i]) begin
-                    for (int k = 0; k < `NUM_FU_STORE; k++) begin
-                        if (f_store_gnt_bus[j][k]) begin
-                            issued_store[k] = next_entries[i];
-                            next_entries[i] = 0;
-                        end
-                    end
-                end
-            end
-            if (br_gnt[i]) begin
-                issued_br = next_entries[i];
-                next_entries[i] = 0;
-            end
-        end
+        // sets all issued entries to be empty
+        next_entries = next_entries & ~next_open_spots; // sussy, next_entries is of type RS_PACKET but next_open_spots is logic
 
-        // Reads in new entries
+        // Reads in new entries (parallelized)
         for (int i = 0; i < N; ++i) begin
-            if (rs_in[i].valid) begin
-                for (int j = 0; j < DEPTH; ++j) begin
-                    if (~next_entries[j].valid) begin
+            if (rs_in[i].valid && dis_entries_bus[i]) begin
+                for (int j = 0; j < DEPTH; j++) begin
+                    if (dis_entries_bus[i][j]) begin
                         next_entries[j] = rs_in[i];
+                        next_open_spots[j] = 0;
                     end
                 end
             end
         end
 
         // next_num_entries logic
-        for (int i = 0; i < `NUM_FU_ALU; i++) begin
-            if (issued_alu[i].valid) begin
-                next_num_entries--;
-            end
-        end
-        for (int i = 0; i < `NUM_FU_MULT; i++) begin
-            if (issued_mult[i].valid) begin
-                next_num_entries--;
-            end
-        end
-        for (int i = 0; i < `NUM_FU_LD; i++) begin
-            if (issued_ld[i].valid) begin
-                next_num_entries--;
-            end
-        end
-        for (int i = 0; i < `NUM_FU_STORE; i++) begin
-            if (issued_store[i].valid) begin
-                next_num_entries--;
-            end
-        end
-        if (issued_br.valid) begin
-            next_num_entries--;
-        end
-        
-        next_num_entries += num_accept;
+        next_num_entries = next_num_entries - (num_alu_issued + num_mult_issued + num_ld_issued + num_store_issued + num_br_issued) + num_accept;
     end
 
     always_ff @(posedge clock) begin
         if (reset) begin
             entries <= 0;
             num_entries <= 0;
+            open_spots <= '1;
         end else begin
             entries <= next_entries;
             num_entries <= next_num_entries;
+            open_spots <= next_open_spots;
         end
     end
 endmodule
