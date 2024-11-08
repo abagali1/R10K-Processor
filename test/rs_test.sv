f97a9be317fcbe8626cfb7dc106fe42329ccc32b
Anup Bagali <anup.bagali1@gmail.com>
2024-11-08 00:26:21 -0500

    initial rs test infra

--- test/rs_test.sv
+++ test/rs_test.sv
@@ -18,7 +18,7 @@
 
 module RS_tb();
 
-    parameter DEPTH = 32;
+    parameter DEPTH = 8;
     parameter N = 3;
     localparam LOG_DEPTH = $clog2(DEPTH);
 
@@ -45,42 +45,21 @@ module RS_tb();
     logic       [$clog2(DEPTH+1)-1:0]   open_entries;
 
     `ifdef DEBUG
-        ROB_ENTRY_PACKET [DEPTH-1:0] debug_entries;
+        RS_PACKET [DEPTH-1:0] debug_entries;
     `endif
 
-    RS_PACKET model_rs[$:(DEPTH)];
+    RS_PACKET model_rs[$:(DEPTH)] = '{DEPTH{0}};
     RS_PACKET decoded_inst_buffer[$:(DEPTH*2)];
     RS_PACKET issued_alu_buffer[$:(`NUM_FU_ALU)];
     RS_PACKET issued_mult_buffer[$:(`NUM_FU_MULT)];
     RS_PACKET issued_ld_buffer[$:(`NUM_FU_LD)];
     RS_PACKET issued_store_buffer[$:(`NUM_FU_STORE)];
-    RS_PACKET issued_br_buffer[$:(`NUM_FU_BR)]; 
+    RS_PACKET issued_br_buffer[$:(`NUM_FU_BR)];
 
     RS #(
         .DEPTH(DEPTH),
         .N(N))
-    dut (
-        .clock(clock),
-        .reset(reset),
-        .rs_in(rs_in),
-        .cdb_in(cdb_in),
-        .br_id(br_id),
-        .br_task(br_task),
-        .fu_alu_busy(fu_alu_busy),
-        .fu_mult_busy(fu_mult_busy),
-        .fu_ld_busy(fu_ld_busy),
-        .fu_store_busy(fu_store_busy),
-        .issued_alu(issued_alu), 
-        .issued_mult(issued_mult),
-        .issued_ld(issued_ld),
-        .issued_store(issued_store),
-        .issued_br(issued_br),
-        .open_entries(open_entries)
-
-        `ifdef DEBUG
-        , .debug_entries(debug_entries)
-        `endif
-    );
+    dut (.*);
 
     always begin
         #(`CLOCK_PERIOD/2.0);
@@ -92,14 +71,22 @@ module RS_tb();
 
         clock = 0;
         reset = 1;
+        clear_inputs();
 
         @(negedge clock);
         @(negedge clock);
         reset = 0;
+        @(negedge clock);
 
         // ------------------------------ Test 1 ------------------------------ //
-        $display("\nTest 1: TODO");
-        add_alu_ops(N);
+        $display("\nTest 1: Dispatch N ALU Instructions");
+        generate_alu_ops(N);
+        @(negedge clock);
+        clear_inputs();
+        fu_alu_busy = '1;
+
+        @(negedge clock);
+
 
         $display("@@@ PASSED ALL TESTS @@@");
         $finish;
@@ -108,7 +95,10 @@ module RS_tb();
 
     // Correctness Verification
     always @(posedge clock) begin
-        #(`CLOCK_PERIOD * 0.2);
+        model_rs_update(); // update model state
+        dispatch_no_br();
+        #(`CLOCK_PERIOD * 0.2); 
+        // verify 
     end
 
     // Helper function to clear inputs to RS
@@ -124,7 +114,57 @@ module RS_tb();
         fu_store_busy = 0;
     endfunction
 
-    function void add_alu_ops(int num);
+    function void model_rs_insert(RS_PACKET in);
+        for(int i=0;i<DEPTH;i++) begin
+            if(!model_rs[i].valid) begin
+                $display("inserting at %d", i);
+                model_rs[i] = in;
+                return;
+            end
+        end
+    endfunction
+
+    function void model_rs_delete(int idx);
+        model_rs[idx] = '0;
+    endfunction
+
+    function int model_rs_count();
+        int count;
+        count = 0;
+        for(int i=0;i<DEPTH;i++) begin
+            if(model_rs[i].valid) begin
+                count++;
+            end
+        end
+        return count;
+    endfunction
+
+    function void model_rs_print();
+        $display("Model RS Entries");
+        $display("\t#\t|\tvalid\t|   dest_idx    |\tt1\t|\tt2\t|\tb_mask\t|\tfu_type\t|");
+        for(int i=0;i<DEPTH;i++) begin
+            //          idx       valid    dest    t1        t2     bmask      op
+            $display("\t%02d\t|\t%01d\t|\t%02d\t|\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, model_rs[i].valid, model_rs[i].t.reg_idx, model_rs[i].t1.reg_idx, model_rs[i].t2.reg_idx, model_rs[i].b_mask, model_rs[i].fu_type);
+        end
+    endfunction
+
+    function void model_rs_update();
+        int num_alu;
+        issued_alu_buffer = {};
+        num_alu = `NUM_FU_ALU - $countones(fu_alu_busy);
+        $display("num_alu %d %d %b", num_alu, $countones(fu_alu_busy), fu_alu_busy);
+        for(int i=0;i<DEPTH;i++) begin
+            if(model_rs[i].valid & model_rs[i].t1.ready & model_rs[i].t2.ready) begin
+                if(num_alu > 0) begin
+                    issued_alu_buffer.push_back(model_rs[i]);
+                    model_rs_delete(i);
+                    num_alu--;
+                end
+            end
+        end
+    endfunction
+
+    function void generate_alu_ops(int num);
         RS_PACKET inst;
         for(int i=0;i<num;i++) begin
             inst.t  = '{reg_idx: 0, valid: 1};
@@ -133,15 +173,24 @@ module RS_tb();
             inst.b_mask = '0;
             inst.fu_type = ALU_INST;
             inst.pred_taken = 0;
+            inst.valid = '1;
+
+            decoded_inst_buffer.push_back(inst);
         end
     endfunction
 
     function void dispatch_no_br();
-        num_accept = N < DEPTH-model_rs.size() ? N : DEPTH-model_rs.size();
+        int num_insts_avail, num_rs_avail;
+        num_rs_avail = DEPTH - model_rs_count();
+        num_insts_avail = decoded_inst_buffer.size();
+
+        num_accept = N < num_insts_avail 
+                        ? N < num_insts_avail ? N : num_insts_avail
+                        : num_insts_avail < num_rs_avail ?  num_insts_avail : num_rs_avail;
 
         for(int i=0;i<num_accept;i++) begin
             rs_in[i] = decoded_inst_buffer.pop_front();
-            
+            model_rs_insert(rs_in[i]);
         end
     endfunction
 
@@ -152,6 +201,8 @@ module RS_tb();
         $display("@@@ Cycle Number: %0d @@@", cycle_number);
         $display("   Time: %0t", $time);
         $display("   Reset: %0d\n", reset);
+        model_rs_print();
+        cycle_number++;
     end
 
 endmodule
