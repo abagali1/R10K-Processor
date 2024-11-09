93dad2c81765cbe50cc6a1fd0fe430712abee001
Rohan Bafna <rbafna@caen-vnc-mi12.engin.umich.edu>
2024-11-09 14:50:35 -0500

    psuedocoded

--- test/br_stack_test.sv
+++ test/br_stack_test.sv
@@ -32,7 +32,8 @@ module br_stack_tb();
     logic                                                       full;
 
 
-    
+    // CHECKPOINT [DEPTH-1] branch_stack_model;
+
    
 
     BR_STACK #(
@@ -47,9 +48,9 @@ module br_stack_tb();
         .in_mt(in_mt),  
         .in_rob_tail(in_rob_tail),  
         .cdb_in(cdb_in),  
-
         .br_task(br_task),   
         .rem_b_id(rem_b_id),   
+        
         .cp_out(cp_out),
         .full(full)
 
@@ -72,7 +73,55 @@ module br_stack_tb();
         reset = 0;
         
 
+        // ------------------------------ Test 1 ------------------------------ //
+        $display("Running Test 1");
+        // if you squash the first branch that came in, 
+        // it should get rid of all the checkpoints
+
+        // add in 2 checkpoints (two branches?)
+        // squash the first branch
+
+        // check that all checkpoints = 0
+
+        // probably will need to add in debug signals to view all the checkpoints at any given time
+        // also maybe output a signal from the psel about which checkpoint idx to check in test bench
+
+
+
+
+
+        // ------------------------------ Test 2 ------------------------------ //
+        $display("Running Test 2");
+        // if you clear one of the checkpoints, it should get rid of the 
+        // corresponding bits in all of the masks of the other checkpoints
+
+        // add in 3 checkpoints with different branch_ids but one is  
+        // clear the second one
+
+
+
+
+
+        
         // ------------------------------ Test 3 ------------------------------ //
+        $display("Running Test 3");
+        // when cdb outputs a register that's updated, recover maptable in 
+        // checkpoint should also update
+
+
+
+        // ------------------------------ Test 4 ------------------------------ //
+        $display("Running Test 4");
+        // squash and try to take in a new checkpoint
+
+
+        
+        // ------------------------------ Test 4 ------------------------------ //
+        $display("Running Test 5");
+        // when you clear a checkpoint and add in a new one,
+        //  want to make sure the bit mask is correct
+
+
         $finish;
 
     end
@@ -91,6 +140,26 @@ module br_stack_tb();
 endmodule
 
 
+function void clear_inputs();
+    valid_assign = '0  
+    in_PC = '0
+    in_mt = '0  
+    in_rob_tail = '0
+    cdb_in = '0
+    br_task = '0
+    rem_b_id = '0
+
+endfunction
+
+function 
+
+function void add_checkpoint(CHECKPOINT data);
+        
+
+
+endfunction
+
+
 
 
 // if you squash the first branch that came in, it should get rid of all the checkpoints
@@ -98,3 +167,5 @@ endmodule
 // when cdb outputs a register that's updated, recover maptable in checkpoint should also update
 // squash and try to take in a new checkpoint
 // when you clear a checkpoint and add in a new one, want to make sure the bit mask is correct
+
+
