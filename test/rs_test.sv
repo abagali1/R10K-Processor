9900e7b3cc902da0c7af58de2908974ca7637430
Anup Bagali <anup.bagali1@gmail.com>
2024-11-11 17:04:25 -0500

    refactor rs inputs

--- test/rs_test.sv
+++ test/rs_test.sv
@@ -22,40 +22,45 @@ module RS_tb();
     parameter N = 3;
     localparam LOG_DEPTH = $clog2(DEPTH);
 
-    logic                                                            clock;
-    logic                                                            reset;
-    RS_PACKET   [N-1:0]                                              rs_in;
-    CDB_PACKET  [N-1:0]                                              cdb_in;
-    logic       [$clog2(N+1)-1:0]                                    num_accept;
-    BR_MASK                                                          br_id;
-    BR_TASK                                                          br_task;
+    logic                                                                            clock;
+    logic                                                                            reset;
+    DECODED_PACKET              [N-1:0]                                              rs_in;
+    FREE_LIST_PACKET            [N-1:0]                                              t_in;
+    MAP_TABLE_PACKET            [N-1:0]                                              t1_in;
+    MAP_TABLE_PACKET            [N-1:0]                                              t2_in;
+    BR_MASK                     [N-1:0]                                              b_mask_in;
 
-    logic       [`NUM_FU_ALU-1:0]                                    fu_alu_busy;
-    logic       [`NUM_FU_MULT-1:0]                                   fu_mult_busy;
-    logic       [`NUM_FU_LD-1:0]                                     fu_ld_busy;
-    logic       [`NUM_FU_STORE-1:0]                                  fu_store_busy;
-    logic       [`NUM_FU_BR-1:0]                                     fu_br_busy;
+    CDB_PACKET                  [N-1:0]                                              cdb_in;
+    logic                       [$clog2(N+1)-1:0]                                    num_accept;
+    BR_MASK                                                                          br_id;
+    BR_TASK                                                                          br_task;
 
-    RS_PACKET   [`NUM_FU_ALU-1:0]                                    issued_alu;
-    RS_PACKET   [`NUM_FU_MULT-1:0]                                   issued_mult;
-    RS_PACKET   [`NUM_FU_LD-1:0]                                     issued_ld;
-    RS_PACKET   [`NUM_FU_STORE-1:0]                                  issued_store;
-    RS_PACKET   [`NUM_FU_BR-1:0]                                     issued_br;
+    logic                       [`NUM_FU_ALU-1:0]                                    fu_alu_busy;
+    logic                       [`NUM_FU_MULT-1:0]                                   fu_mult_busy;
+    logic                       [`NUM_FU_LD-1:0]                                     fu_ld_busy;
+    logic                       [`NUM_FU_STORE-1:0]                                  fu_store_busy;
+    logic                       [`NUM_FU_BR-1:0]                                     fu_br_busy;
 
-    logic       [$clog2(N+1)-1:0]                                    open_entries;
+    RS_PACKET                   [`NUM_FU_ALU-1:0]                                    issued_alu;
+    RS_PACKET                   [`NUM_FU_MULT-1:0]                                   issued_mult;
+    RS_PACKET                   [`NUM_FU_LD-1:0]                                     issued_ld;
+    RS_PACKET                   [`NUM_FU_STORE-1:0]                                  issued_store;
+    RS_PACKET                   [`NUM_FU_BR-1:0]                                     issued_br;
 
-    `ifdef DEBUG
-        RS_PACKET [DEPTH-1:0]                                        debug_entries;
-        logic     [DEPTH-1:0]                                        debug_open_spots;
-        logic     [DEPTH-1:0]                                        debug_other_sig;
-        logic     [N-1:0][DEPTH-1:0]                                 debug_dis_entries_bus;
-        logic     [$clog2(DEPTH+1)-1:0]                              debug_open_entries;
-        logic     [DEPTH-1:0]                                        debug_all_issued_insts;
-        logic     [`NUM_FU_ALU-1:0][DEPTH-1:0]                       debug_alu_issued_bus;
-        logic     [DEPTH-1:0]                                        debug_alu_req;
-        logic     [`NUM_FU_ALU-1:0][`NUM_FU_ALU-1:0]                 debug_alu_fu_gnt_bus;
-        logic     [`NUM_FU_ALU-1:0][DEPTH-1:0]                       debug_alu_inst_gnt_bus;
-    `endif
+    logic                       [$clog2(N+1)-1:0]                                    open_entries;
+
+`ifdef DEBUG
+    RS_PACKET                   [DEPTH-1:0]                                          debug_entries;
+    logic                       [DEPTH-1:0]                                          debug_open_spots;
+    logic                       [DEPTH-1:0]                                          debug_other_sig;
+    logic                       [N-1:0][DEPTH-1:0]                                   debug_dis_entries_bus;
+    logic                       [$clog2(DEPTH+1)-1:0]                                debug_open_entries;
+    logic                       [DEPTH-1:0]                                          debug_all_issued_insts;
+    logic                       [`NUM_FU_ALU-1:0][DEPTH-1:0]                         debug_alu_issued_bus;
+    logic                       [DEPTH-1:0]                                          debug_alu_req;
+    logic                       [`NUM_FU_ALU-1:0][`NUM_FU_ALU-1:0]                   debug_alu_fu_gnt_bus;
+    logic                       [`NUM_FU_ALU-1:0][DEPTH-1:0]                         debug_alu_inst_gnt_bus;
+`endif
 
     RS_PACKET model_rs[$:(DEPTH)] = '{DEPTH{0}};
     RS_PACKET decoded_inst_buffer[$:(DEPTH*2)];
@@ -84,17 +89,30 @@ module RS_tb();
         clear_inputs();
 
         @(negedge clock);
-        @(negedge clock);
         reset = 0;
         @(negedge clock); // 0
 
         // ------------------------------ Test 1 ------------------------------ //
-        $display("\nTest 1: Dispatch N ALU Instructions");
-        generate_alu_ops(N);
-        @(negedge clock); // 1
-        fu_alu_busy = '1;
-        @(negedge clock); // 2
-        @(negedge clock); // 3
+        $display("\nTest 1: Basic Dispatch/Issue N ALU Instructions");
+        generate_ops(N, ALU_INST);
+        @(negedge clock); // dispatch
+        fu_alu_busy = 0;
+        @(negedge clock); // issue
+        
+        reset = 1;
+        clear_inputs();
+        @(negedge clock); // verify issued
+
+        // ------------------------------ Test 2 ------------------------------ //
+        $display("\nTest 2: Basic Dispatch/Issue N MULT Instructions");
+        reset = 0;
+        generate_ops(N, MULT_INST);
+        @(negedge clock); // dispatch
+        fu_mult_busy = 0;
+        @(negedge clock); // issue
+        @(negedge clock); // verify issued
+
+        clear_inputs();
 
 
         $display("@@@ PASSED ALL TESTS @@@");
@@ -122,6 +140,10 @@ module RS_tb();
     function void clear_inputs();
         num_accept = 0;
         rs_in = 0;
+        t_in = 0;
+        t1_in = 0;
+        t2_in = 0;
+        b_mask_in = 0;
         cdb_in = 0;
         br_id = 0;
         br_task = 0;
@@ -134,16 +156,16 @@ module RS_tb();
     function void model_rs_insert(RS_PACKET in, int lsb);
         if(lsb) begin
             for(int i=0;i<DEPTH;i++) begin
-                if(!model_rs[i].valid) begin
-                    $display("inserting [(%b) (%02d)] at %d", in.valid, in.t.reg_idx, i);
+                if(!model_rs[i].decoded_vals.valid) begin
+                    $display("inserting [(%b) (%02d)] at %d", in.decoded_vals.valid, in.t.reg_idx, i);
                     model_rs[i] = in;
                     return;
                 end
             end
         end else begin
             for(int i=DEPTH-1;i>=0;i--) begin
-                if(!model_rs[i].valid) begin
-                    $display("inserting [(%b) (%02d)] at %d", in.valid, in.t.reg_idx, i);
+                if(!model_rs[i].decoded_vals.valid) begin
+                    $display("inserting [(%b) (%02d)] at %d", in.decoded_vals.valid, in.t.reg_idx, i);
                     model_rs[i] = in;
                     return;
                 end
@@ -155,7 +177,7 @@ module RS_tb();
         RS_PACKET res;
         if(lsb) begin
             for(int i=0;i<DEPTH;i++) begin
-                if(model_rs[i].fu_type == fu_type && model_rs[i].valid && model_rs[i].t1.ready && model_rs[i].t2.ready) begin
+                if(model_rs[i].decoded_vals.fu_type == fu_type && model_rs[i].decoded_vals.valid && model_rs[i].t1.ready && model_rs[i].t2.ready) begin
                     res = model_rs[i];
                     model_rs_delete(i);
                     return res;
@@ -163,7 +185,7 @@ module RS_tb();
             end
         end else begin
             for(int i=DEPTH-1;i>=0;i--) begin
-                if(model_rs[i].fu_type == fu_type && model_rs[i].valid && model_rs[i].t1.ready && model_rs[i].t2.ready) begin
+                if(model_rs[i].decoded_vals.fu_type == fu_type && model_rs[i].decoded_vals.valid && model_rs[i].t1.ready && model_rs[i].t2.ready) begin
                     res = model_rs[i];
                     model_rs_delete(i);
                     return res;
@@ -181,7 +203,7 @@ module RS_tb();
         int count;
         count = 0;
         for(int i=0;i<DEPTH;i++) begin
-            if(model_rs[i].valid) begin
+            if(model_rs[i].decoded_vals.valid) begin
                 count++;
             end
         end
@@ -194,8 +216,8 @@ module RS_tb();
 
     function void model_rs_check();
         for(int i=0;i<`NUM_FU_ALU;i++) begin
-            if(issued_alu_buffer[i].valid != issued_alu[i].valid) begin
-                $display("ISSUE PACKET MISMATCH AT %d %02d %02d", i, issued_alu_buffer[i].valid, issued_alu[i].valid);
+            if(issued_alu_buffer[i].decoded_vals.valid != issued_alu[i].decoded_vals.valid || issued_alu_buffer[i].t != issued_alu[i].t) begin
+                $display("ISSUE PACKET MISMATCH AT %d %02d %02d", i, issued_alu_buffer[i].decoded_vals.valid, issued_alu[i].decoded_vals.valid);
                 $finish;
             end
         end
@@ -216,6 +238,10 @@ module RS_tb();
         while(num_alu_ready > 0) begin
             issued_packet = model_rs_pop(num_alu_issued % 2, ALU_INST);
 
+            if(!issued_packet.decoded_vals.valid) begin
+                break;
+            end
+
             fu_issued_idx = num_alu_issued % 2 ? lsb(fu_alu_ready, `NUM_FU_ALU) : msb(fu_alu_ready, `NUM_FU_ALU);
 
             fu_alu_ready[fu_issued_idx] = 0;
@@ -226,22 +252,24 @@ module RS_tb();
         end
     endfunction
 
-    function void generate_alu_ops(int num);
+    function void generate_ops(int num, FU_TYPE fu_type);
         RS_PACKET inst;
         for(int i=0;i<num;i++) begin
             inst.t  = '{reg_idx: i, valid: 1};
             inst.t1 = '{reg_idx: 0, valid: 1, ready: 1};
             inst.t2 = '{reg_idx: 0, valid: 1, ready: 1};
             inst.b_mask = '0;
-            inst.fu_type = ALU_INST;
-            inst.pred_taken = 0;
-            inst.valid = '1;
+
+            inst.decoded_vals = 0;
+            inst.decoded_vals.valid = 1;
+            inst.decoded_vals.fu_type = fu_type;
 
             decoded_inst_buffer.push_back(inst);
         end
     endfunction
 
     function void dispatch_no_br();
+        RS_PACKET packet;
         int num_insts_avail, num_rs_avail;
 
         rs_in = 0;
@@ -253,8 +281,14 @@ module RS_tb();
                         : num_insts_avail < num_rs_avail ?  num_insts_avail : num_rs_avail;
 
         for(int i=0;i<num_accept;i++) begin
-            rs_in[i] = decoded_inst_buffer.pop_front();
-            model_rs_insert(rs_in[i], i % 2);
+            packet = decoded_inst_buffer.pop_front();
+            rs_in[i] = packet.decoded_vals;
+            t_in[i] = packet.t;
+            t1_in[i] = packet.t1;
+            t2_in[i] = packet.t2;
+            b_mask_in[i] = packet.b_mask;
+
+            model_rs_insert(packet, i % 2);
         end
     endfunction
 
@@ -269,9 +303,9 @@ module RS_tb();
 
 
         for(int i=`NUM_FU_ALU-1;i>=0;i--) begin
-            $write("\t%02d\t|\t%01d\t|\t%02d\t|\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, issued_alu_buffer[i].valid, issued_alu_buffer[i].t.reg_idx, issued_alu_buffer[i].t1.reg_idx, issued_alu_buffer[i].t2.reg_idx, issued_alu_buffer[i].b_mask, issued_alu_buffer[i].fu_type);
+            $write("\t%02d\t|\t%01d\t|\t%02d\t |\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, issued_alu_buffer[i].decoded_vals.valid, issued_alu_buffer[i].t.reg_idx, issued_alu_buffer[i].t1.reg_idx, issued_alu_buffer[i].t2.reg_idx, issued_alu_buffer[i].b_mask, issued_alu_buffer[i].decoded_vals.fu_type);
             $write("\t\t");
-            $write("\t%02d\t|\t%01d\t|\t%02d\t|\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, issued_alu[i].valid, issued_alu[i].t.reg_idx, issued_alu[i].t1.reg_idx, issued_alu[i].t2.reg_idx, issued_alu[i].b_mask, issued_alu[i].fu_type);
+            $write("\t%02d\t|\t%01d\t|\t%02d\t |\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, issued_alu[i].decoded_vals.valid, issued_alu[i].t.reg_idx, issued_alu[i].t1.reg_idx, issued_alu[i].t2.reg_idx, issued_alu[i].b_mask, issued_alu[i].decoded_vals.fu_type);
             $write("\n");
         end
 
@@ -299,10 +333,10 @@ module RS_tb();
 
         for(int i=DEPTH-1;i>=0;i--) begin
             //          idx       valid    dest    t1        t2     bmask      op
-            $write("\t%02d\t|\t%01d\t|\t%02d\t|\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, model_rs[i].valid, model_rs[i].t.reg_idx, model_rs[i].t1.reg_idx, model_rs[i].t2.reg_idx, model_rs[i].b_mask, model_rs[i].fu_type);
+            $write("\t%02d\t|\t%01d\t|\t%02d\t |\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, model_rs[i].decoded_vals.valid, model_rs[i].t.reg_idx, model_rs[i].t1.reg_idx, model_rs[i].t2.reg_idx, model_rs[i].b_mask, model_rs[i].decoded_vals.fu_type);
             `ifdef DEBUG
                 $write("\t\t");
-                $write("\t%02d\t|\t%01d\t|\t%02d\t|\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, debug_entries[i].valid, debug_entries[i].t.reg_idx, debug_entries[i].t1.reg_idx, debug_entries[i].t2.reg_idx, debug_entries[i].b_mask, debug_entries[i].fu_type);
+                $write("\t%02d\t|\t%01d\t|\t%02d\t |\t%02d\t|\t%02d\t|\t%04b\t|\t%01d\t|", i, debug_entries[i].decoded_vals.valid, debug_entries[i].t.reg_idx, debug_entries[i].t1.reg_idx, debug_entries[i].t2.reg_idx, debug_entries[i].b_mask, debug_entries[i].decoded_vals.fu_type);
             `endif
             $write("\n");
         end
@@ -311,7 +345,7 @@ module RS_tb();
             $display("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tDispatching Entries Bus");
             for(int i=0;i<N;i++) begin
                 $write("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t");
-                $write("[%b (%02d)] [(%b) (%02d)]", debug_dis_entries_bus[i], msb(debug_dis_entries_bus[i], DEPTH), rs_in[i].valid, rs_in[i].t.reg_idx);
+                $write("[%b (%02d)] [(%b) (%02d)]", debug_dis_entries_bus[i], msb(debug_dis_entries_bus[i], DEPTH), rs_in[i].valid, t_in[i].reg_idx);
                 $write("\n");
             end
             $display();
