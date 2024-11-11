190815b3c1c3bcaf6dfa3c9e2eebb3972ee2c71c
Anup Bagali <anup.bagali1@gmail.com>
2024-11-11 04:07:59 -0500

    debugging issuing logic

--- verilog/rs_psel.sv
+++ verilog/rs_psel.sv
@@ -3,17 +3,20 @@ module rs_psel #(
     parameter NUM_FU
 )
 (
-    input logic [DEPTH-1:0] inst_req, // which insts can be issued for this FU
-    input logic [NUM_FU-1:0] fu_req, // which FUs (for this op) are ready
-    output logic [$clog2(NUM_FU+1)-1:0] num_issued,
-    output logic [NUM_FU-1:0] fu_issued_insts, // issued insts w.r.t current FU
-    output logic [DEPTH-1:0] all_issued_insts  // issued insts w.r.t all RS entires
+    input logic             [DEPTH-1:0]                         inst_req, // which insts can be issued for this FU
+    input logic             [NUM_FU-1:0]                        fu_req, // which FUs (for this op) are ready
+    output logic            [$clog2(NUM_FU+1)-1:0]              num_issued,
+    output logic            [NUM_FU-1:0][DEPTH-1:0]             fu_issued_insts, // issued insts w.r.t current FU
+    output logic            [DEPTH-1:0]                         all_issued_insts  // issued insts w.r.t all RS entires
+
+    `ifdef DEBUG
+        ,   output logic    [NUM_FU-1:0][NUM_FU-1:0]            debug_fu_gnt_bus,
+            output logic    [NUM_FU-1:0][DEPTH-1:0]             debug_inst_gnt_bus
+    `endif
 );
 
-    logic [DEPTH-1:0] inst_gnt; // many hot encoded
-    logic [NUM_FU-1:0][DEPTH-1:0] inst_gnt_bus; // one-hot
-
-    logic [NUM_FU-1:0] fu_gnt;
+    logic [NUM_FU-1:0]             fu_gnt;
+    logic [NUM_FU-1:0][DEPTH-1:0]  inst_gnt_bus; // one-hot
     logic [NUM_FU-1:0][NUM_FU-1:0] fu_gnt_bus;
 
     psel_gen #(
@@ -21,7 +24,7 @@ module rs_psel #(
         .REQS(NUM_FU))
     inst_psel (
         .req(inst_req),
-        .gnt(inst_gnt),
+        .gnt(all_issued_insts),
         .gnt_bus(inst_gnt_bus),
         .empty()
     );
@@ -36,17 +39,21 @@ module rs_psel #(
         .empty()
     );
 
-    always_comb begin
-        all_issued_insts = '0;
-        num_issued = '0;
-        fu_issued_insts = '0;
+    `ifdef DEBUG
+        assign debug_fu_gnt_bus = fu_gnt_bus;
+        assign debug_inst_gnt_bus = inst_gnt_bus;
+    `endif
 
+    always_comb begin
         for(int i=0;i<NUM_FU;i++) begin
-            if(fu_gnt_bus[i] && inst_gnt_bus[i]) begin 
-                all_issued_insts |= inst_gnt_bus[i];
-                fu_issued_insts |= fu_gnt_bus[i];
+            for(int j=0;j<NUM_FU;j++) begin
+                fu_issued_insts[j] = fu_gnt_bus[i][j] ? inst_gnt_bus[i] : '0;
             end
         end
+    end
+
+    always_comb begin
+        num_issued = '0;
 
         for(int i=0;i<DEPTH;i++) begin
             if(all_issued_insts[i]) begin
