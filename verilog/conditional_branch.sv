e422f190a7452e21b18f54f589151f1771dd621f
John Favret <johnfav@umich.edu>
2024-11-11 20:17:22 -0500

    alu test case

--- verilog/conditional_branch.sv
+++ verilog/conditional_branch.sv
@@ -9,26 +9,24 @@ module conditional_branch (
     input logic         stall, // I don't think we should ever stall this
     input logic         rd_in,
 
-    output FU_PACKET    fu_pack;
+    output FU_PACKET    fu_pack,
+    output logic        data_ready
 );
     FU_PACKET out, next_out;
     DATA rs1, rs2;
     logic take;
 
-    assign rs1 = is_pack.rs1_value;
-    assign rs2 = is_pack.rs2_value;
-
     assign fu_pack = out;
 
     // Combinational logic for choosing taken
     always_comb begin
-        case (is_pack.rs_packet.inst.b.funct3)
-            3'b000:  take = signed'(rs1) == signed'(rs2); // BEQ
-            3'b001:  take = signed'(rs1) != signed'(rs2); // BNE
-            3'b100:  take = signed'(rs1) <  signed'(rs2); // BLT
-            3'b101:  take = signed'(rs1) >= signed'(rs2); // BGE
-            3'b110:  take = rs1 < rs2;                    // BLTU
-            3'b111:  take = rs1 >= rs2;                   // BGEU
+        case (is_pack.decoded_vals.decoded_vals.inst.b.funct3)
+            3'b000:  take = signed'(is_pack.rs1_value) == signed'(is_pack.rs2_value); // BEQ
+            3'b001:  take = signed'(is_pack.rs1_value) != signed'(is_pack.rs2_value); // BNE
+            3'b100:  take = signed'(is_pack.rs1_value) <  signed'(is_pack.rs2_value); // BLT
+            3'b101:  take = signed'(is_pack.rs1_value) >= signed'(is_pack.rs2_value); // BGE
+            3'b110:  take = is_pack.rs1_value < is_pack.rs2_value;                    // BLTU
+            3'b111:  take = is_pack.rs1_value >= is_pack.rs2_value;                   // BGEU
             default: take = `FALSE;
         endcase
     end
@@ -37,8 +35,10 @@ module conditional_branch (
     always_comb begin
         if (stall) begin
             next_out = out;
+            data_ready = rd_in;
         end else begin
-            next_out = '{alu_result: '0, is_pack: is_pack, take_conditional: take};
+            next_out = '{alu_result: '0, decoded_vals: is_pack.decoded_vals, take_conditional: take};
+            data_ready = '0;
         end
     end
 
