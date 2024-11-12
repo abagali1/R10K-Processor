66184361469bd4cb136025979c2a5ea46cae949c
Gannon R Smith <gansmith@caen-vnc-mi08.engin.umich.edu>
2024-11-12 00:05:20 -0500

    adds MT debugger output

--- verilog/map_table.sv
+++ verilog/map_table.sv
@@ -76,9 +76,16 @@ module map_table #(
 
     `ifdef DEBUG_MT
         always @(posedge clock) begin
-            $display("=================== MAP TABLE ===================");
+            $display("=================== MAP TABLE ===================\n");
+            $display("  Entries:");
+            $display("  ---------------------------------------------");
+            $display("  |   Index  |  Reg_idx |   Ready  |   Valid  |");
+            $display("  ---------------------------------------------");
+            for (int i = 1; i <= DEPTH; i++) begin
+                $display("  |    %2d    |    %2d    |    %2d    |    %2d    |", i, entries[i].reg_idx, entries[i].ready, entries[i].valid);
+                $display("  ---------------------------------------------");
+            end
             $display("");
-
         end
     `endif
 
