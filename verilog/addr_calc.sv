da1665a0068aae4fde45cc46d4a25b3dc3dcaaa6
Anup Bagali <anup.bagali1@gmail.com>
2024-11-12 23:17:33 -0500

    updated addr_calc to use op select

--- verilog/addr_calc.sv
+++ verilog/addr_calc.sv
@@ -12,7 +12,33 @@ module addr_calc (
     output              data_ready
 );
     FU_PACKET out_packet, next_out_packet;
-    DATA base, offset;
+    DATA opa, opb;
+
+    assign fu_pack = out;
+
+    // ALU opA mux
+    always_comb begin
+        case (is_pack.decoded_vals.decoded_vals.opa_select)
+            OPA_IS_RS1:  opa = is_pack.rs1_value;
+            OPA_IS_NPC:  opa = is_pack.decoded_vals.decoded_vals.NPC;
+            OPA_IS_PC:   opa = is_pack.decoded_vals.decoded_vals.PC;
+            OPA_IS_ZERO: opa = 0;
+            default:     opa = 32'hdeadface; // dead face
+        endcase
+    end
+
+    // ALU opB mux
+    always_comb begin
+        case (is_pack.decoded_vals.decoded_vals.opb_select)
+            OPB_IS_RS2:   opb = is_pack.rs2_value;
+            OPB_IS_I_IMM: opb = `RV32_signext_Iimm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_S_IMM: opb = `RV32_signext_Simm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_B_IMM: opb = `RV32_signext_Bimm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_U_IMM: opb = `RV32_signext_Uimm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_J_IMM: opb = `RV32_signext_Jimm(is_pack.decoded_vals.decoded_vals.inst);
+            default:      opb = 32'hfacefeed; // face feed
+        endcase
+    end
 
     always_ff @(posedge clock) begin
         if (reset) begin
@@ -25,11 +51,7 @@ module addr_calc (
     end
 
     assign next_out_packet.decoded_vals = is_pack.decoded_vals;
-
-    assign base = (is_pack.decoded_vals.decoded_vals.uncond_branch) ? is_pack.decoded_vals.decoded_vals.PC : is_pack.rs1_value;
-    assign offset = (is_pack.decoded_vals.decoded_vals.uncond_branch) ? `RV32_signext_Jimm(is_pack.decoded_vals.decoded_vals.inst) :
-                    (is_pack.decoded_vals.decoded_vals.wr_mem) ? `RV32_signext_Simm(is_pack.decoded_vals.decoded_vals.inst) : `RV32_signext_Iimm(rs_in.inst);   //covers JALR
-    assign next_out_packet.alu_result = base + offset;
+    assign next_out_packet.alu_result = opa + opb;
 
     assign data_ready = ~stall & rd_in;
 endmodule
