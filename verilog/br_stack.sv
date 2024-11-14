1efe65aa1a989a1579823b94d841b440e64fbfec
Vijaya B Kukutla <vkukutla@caen-vnc-mi09.engin.umich.edu>
2024-11-13 22:24:32 -0500

    debug signal for integration for br_stack

--- verilog/br_stack.sv
+++ verilog/br_stack.sv
@@ -127,4 +127,17 @@ module br_stack #(
         end
     end
 
+    `ifdef DEBUG
+        always @(posedge clock) begin
+            $display("============== BRANCH STACK ==============\n");
+            $display("  Entries:");
+            $display("-------------------------------------");
+            $display("i | b_id |  b_mask | rec_PC | fl_head | rob_tail  |");
+            for (int i = 0; i < DEPTH; i++) begin
+                $display("%02d|  %02d  |   %02d   |   %02d   |  %02d  |   %01d   |", i, entries[i].b_id, entries[i].b_mask, entries[i].rec_PC, entries[i].fl_head, entries[i].rob_tail);
+            end
+            $display("");
+        end
+    `endif
+
 endmodule
