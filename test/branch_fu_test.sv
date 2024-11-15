20ec7fa4c5c1503bf692e6a8fc6e53018310647b
Anup Bagali <anup.bagali1@gmail.com>
2024-11-15 17:49:50 -0500

    removing PC from br_stack

---
+++ test/branch_fu_test.sv
@@ -0,0 +1,11 @@
+/////////////////////////////////////////////////////////////////////////
+//                                                                     //
+//   Modulename :  branch_fu_test.sv                                   //
+//                                                                     //
+//  Description :  Testbench module for the branch FU                  //
+//                                                                     //
+//                                                                     //
+/////////////////////////////////////////////////////////////////////////
+
+`include "sys_defs.svh"
+`include "ISA.svh"
