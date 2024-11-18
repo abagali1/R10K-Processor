057d4b03ba20dae08cba1c8aacd8cdff635fe97e
Anup Bagali <anup.bagali1@gmail.com>
2024-11-17 22:48:59 -0500

    abstracted decoder for synth

--- verilog/decoder.sv
+++ verilog/decoder.sv
@@ -9,18 +9,19 @@
 
 // Decode an instruction: generate useful datapath control signals by matching the RISC-V ISA
 // This module is purely combinational
-module decoder (
-    input INST  inst,
-    input logic valid, // when low, ignore inst. Output will look like a NOP
+module decoder 
+(
+    input INST_PACKET inst,
 
-    output ALU_OPA_SELECT opa_select,
-    output ALU_OPB_SELECT opb_select,
-    output logic          has_dest, // if there is a destination register
-    output ALU_FUNC       alu_func,
-    output logic          mult, rd_mem, wr_mem, cond_branch, uncond_branch,
-    output logic          csr_op, // used for CSR operations, we only use this as a cheap way to get the return code out
-    output logic          halt,   // non-zero on a halt
-    output logic          illegal // non-zero on an illegal instruction
+    output FU_TYPE         fu_type,
+    output ALU_OPA_SELECT  opa_select,
+    output ALU_OPB_SELECT  opb_select,
+    output logic           has_dest, // if there is a destination register
+    output ALU_FUNC        alu_func,
+    output logic           mult, rd_mem, wr_mem, cond_branch, uncond_branch,
+    output logic           csr_op, // used for CSR operations, we only use this as a cheap way to get the return code out
+    output logic           halt,   // non-zero on a halt
+    output logic           illegal // non-zero on an illegal instruction
 );
 
     // Note: I recommend using an IDE's code folding feature on this block
@@ -39,9 +40,10 @@ module decoder (
         uncond_branch = `FALSE;
         halt          = `FALSE;
         illegal       = `FALSE;
+        fu_type       = ALU_INST;
 
-        if (valid) begin
-            casez (inst)
+        if (inst.valid) begin
+            casez (inst.inst)
                 `RV32_LUI: begin
                     has_dest   = `TRUE;
                     opa_select = OPA_IS_ZERO;
@@ -57,23 +59,27 @@ module decoder (
                     opa_select    = OPA_IS_PC;
                     opb_select    = OPB_IS_J_IMM;
                     uncond_branch = `TRUE;
+                    fu_type       = BR_INST;
                 end
                 `RV32_JALR: begin
                     has_dest      = `TRUE;
                     opa_select    = OPA_IS_RS1;
                     opb_select    = OPB_IS_I_IMM;
                     uncond_branch = `TRUE;
+                    fu_type       = BR_INST;
                 end
                 `RV32_BEQ, `RV32_BNE, `RV32_BLT, `RV32_BGE,
                 `RV32_BLTU, `RV32_BGEU: begin
                     opa_select  = OPA_IS_PC;
                     opb_select  = OPB_IS_B_IMM;
                     cond_branch = `TRUE;
+                    fu_type     = BR_INST;
                     // stage_ex uses inst.b.funct3 as the branch function
                 end
                 `RV32_MUL, `RV32_MULH, `RV32_MULHSU, `RV32_MULHU: begin
                     has_dest   = `TRUE;
                     mult       = `TRUE;
+                    fu_type    = MULT_INST;
                     // stage_ex uses inst.r.funct3 as the mult function
                 end
                 `RV32_LB, `RV32_LH, `RV32_LW,
@@ -81,11 +87,13 @@ module decoder (
                     has_dest   = `TRUE;
                     opb_select = OPB_IS_I_IMM;
                     rd_mem     = `TRUE;
+                    fu_type    = LD_INST;
                     // stage_ex uses inst.r.funct3 as the load size and signedness
                 end
                 `RV32_SB, `RV32_SH, `RV32_SW: begin
                     opb_select = OPB_IS_S_IMM;
                     wr_mem     = `TRUE;
+                    fu_type    = STORE_INST;
                     // stage_ex uses inst.r.funct3 as the store size
                 end
                 `RV32_ADDI: begin
@@ -182,6 +190,7 @@ module decoder (
                 end
         endcase // casez (inst)
         end // if (valid)
+        
     end // always
 
 endmodule // decoder
