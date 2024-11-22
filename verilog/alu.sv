937bd19898b9182bc05bf92692d57474e1354c19
Anup Bagali <anup.bagali1@gmail.com>
2024-11-22 04:46:01 -0500

    move immediate logic back to ALU

--- verilog/alu.sv
+++ verilog/alu.sv
@@ -11,14 +11,34 @@ module alu (
     output FU_PACKET    fu_pack,
     output logic        data_ready
 );
-    DATA result;
-    DATA opa;
-    DATA opb;
+    DATA result, opa, opb;
     FU_PACKET out, next_out;
 
     assign fu_pack = out;
-    assign opa = is_pack.rs1_value;
-    assign opb = is_pack.rs2_value;
+
+    // ALU opA mux
+    always_comb begin
+        case (is_pack.decoded_vals.decoded_vals.opa_select)
+            OPA_IS_RS1:  opa = is_pack.rs1_value;
+            OPA_IS_NPC:  opa = is_pack.decoded_vals.decoded_vals.NPC;
+            OPA_IS_PC:   opa = is_pack.decoded_vals.decoded_vals.PC;
+            OPA_IS_ZERO: opa = 0;
+            default:     opa = 32'hdeadface; // dead face
+        endcase
+    end
+
+    // ALU opB mux
+    always_comb begin
+        case (is_pack.decoded_vals.decoded_vals.opb_select)
+            OPB_IS_RS2:   opb = is_pack.rs2_value;
+            OPB_IS_I_IMM: opb = `RV32_signext_Iimm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_S_IMM: opb = `RV32_signext_Simm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_B_IMM: opb = `RV32_signext_Bimm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_U_IMM: opb = `RV32_signext_Uimm(is_pack.decoded_vals.decoded_vals.inst);
+            OPB_IS_J_IMM: opb = `RV32_signext_Jimm(is_pack.decoded_vals.decoded_vals.inst);
+            default:      opb = 32'hfacefeed; // face feed
+        endcase
+    end
 
     // ALU Compute Result
     always_comb begin
@@ -55,16 +75,5 @@ module alu (
             out         <= '0;
         end
     end
-
-    `ifdef DEBUG
-        always_ff @(posedge clock) begin
-            $display("============== ALU ================");
-
-            $display("   Packet Inst: %0d, Result: %0x, Data_ready: %0d, Stall: %0d", out.decoded_vals.decoded_vals.inst, out.result, data_ready, stall);
-            
-        end
-    `endif
-
-    
 
 endmodule
