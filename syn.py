412713907c4c0f7fcaf10dcd6bb598f7455f8b10
Anup Bagali <anup.bagali1@gmail.com>
2024-12-08 21:41:58 -0500

    fix script

--- syn.py
+++ syn.py
@@ -16,11 +16,11 @@ inc = 1
 
 while (high_p - low_p) > 0.5:
     mid = (high_p + low_p) / 2
+    print(f"Synthesizing CLOCK_PERIOD={mid}")
     os.system(f'sed -i "99s/.*/export CLOCK_PERIOD={mid}/" Makefile')
     subprocess.call(["make", "nuke"], stdout=subprocess.DEVNULL)
     subprocess.call(["make", "syn_simv"], stdout=subprocess.DEVNULL)
 
-    print(f"Synthesizing CLOCK_PERIOD={mid}")
     violated = os.system(f"grep -E \"VIOLATED\" tmp/{mid}.rep") # 0 if violated, 256 else
 
     if(violated == 0):
